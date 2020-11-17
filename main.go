f2033138a366b7baa61217cb9d91215d7377abff
Jordan Anderson <me@jba.io>
2020-11-17 13:12:31 -0600

    Moved font to a global var. Initialized in main(). Dedicated time surface and textures funcs. Ripped out most of my original swipelocker code

--- main.go
+++ main.go
@@ -16,18 +16,46 @@ var (
 	fontFile        string
 	screenHeight    int32 = 480
 	screenWidth     int32 = 640
+	font            *ttf.Font
 )
+
+func getTimeSurface() *sdl.Surface {
+	fontSurface, err := font.RenderUTF8Solid(time.Now().Format("03:04:05PM"), sdl.Color{
+		R: 0,
+		G: 0,
+		B: 0,
+		A: 255,
+	})
+	if err != nil {
+		log.Fatalln("can't render font:", err)
+	}
+	return fontSurface
+}
+
+func getTimeTextureFromSurface(renderer *sdl.Renderer, surface *sdl.Surface) *sdl.Texture {
+	fontText, err := renderer.CreateTextureFromSurface(surface)
+	if err != nil {
+		log.Fatalln("Error creating font texture:", err)
+	}
+	surface.Free()
+	return fontText
+}
+
+func getTimeTexture(renderer *sdl.Renderer) *sdl.Texture {
+	surface := getTimeSurface()
+	fontText, err := renderer.CreateTextureFromSurface(surface)
+	if err != nil {
+		log.Fatalln("Error creating font texture:", err)
+	}
+	surface.Free()
+	return fontText
+}
 
 func run() (err error) {
 	var window *sdl.Window
 	//var surface *sdl.Surface
 	var pngImage *sdl.Surface
 	//var otherImage *sdl.Surface
-
-	err = ttf.Init()
-	if err != nil {
-		log.Fatalln("Error iniitializing font lib")
-	}
 
 	if err = sdl.Init(sdl.INIT_VIDEO); err != nil {
 		log.Println(err)
@@ -49,48 +77,37 @@ func run() (err error) {
 	}
 	defer window.Destroy()
 
-	// Load fonts
-	font, err := ttf.OpenFont(fontFile, 64)
-	if err != nil {
-		log.Fatalln("can't open font:", err)
-	}
+	// Initial surfaces to get size
+	timeSurface := getTimeSurface()
 
-	fontSurface, err := font.RenderUTF8Blended("Drag Up", sdl.Color{
-		R: 255,
-		G: 0,
-		B: 0,
-		A: 255,
-	})
-	if err != nil {
-		log.Fatalln("can't render font:", err)
-	}
-	fontText, err := renderer.CreateTextureFromSurface(fontSurface)
-	if err != nil {
-		log.Fatalln("Error creating font texture:", err)
-	}
-	fontSurface.Free()
+	// Calculate perfect center, based on font and screen size
+	var screenCenterY int32 = screenHeight/2 - timeSurface.H/2
+	var screenCenterX int32 = screenWidth/2 - timeSurface.W/2
+
+	// Define sctions of the screen
+	fullRect := &sdl.Rect{X: 0, Y: 0, W: screenWidth, H: screenHeight}
+	centerRect := &sdl.Rect{X: screenCenterX, Y: screenCenterY, W: timeSurface.W, H: timeSurface.H}
+
+	// Font surface to texture
+	timeTexture := getTimeTextureFromSurface(renderer, timeSurface)
 
 	// Load a PNG image
 	if pngImage, err = img.Load(backgroundImage); err != nil {
 		log.Println(err)
 		return err
 	}
-	defer pngImage.Free()
 
-	imageText, err := renderer.CreateTextureFromSurface(pngImage)
+	imageTexture, err := renderer.CreateTextureFromSurface(pngImage)
 	if err != nil {
 		log.Println(err)
 		return err
 	}
+	pngImage.Free()
 
-	renderer.Copy(imageText, nil, &sdl.Rect{X: 0, Y: 0, W: screenWidth, H: screenHeight})
+	// Copy background to the full screen, then text on top, to the center
+	renderer.Copy(imageTexture, nil, fullRect)
+	renderer.Copy(timeTexture, nil, centerRect)
 
-	// Calculate perfect center, based on font and screen size
-	var textY int32 = screenHeight/2 - fontSurface.H/2
-	var textX int32 = screenWidth/2 - fontSurface.W/2
-
-	fontRect := &sdl.Rect{X: textX, Y: textY, W: fontSurface.W, H: fontSurface.H}
-	renderer.Copy(fontText, nil, fontRect)
 	renderer.Present()
 
 	window.Show()
@@ -104,28 +121,11 @@ func run() (err error) {
 
 		renderer.Clear()
 
-		renderer.Copy(imageText, nil, &sdl.Rect{X: 0, Y: 0, W: screenWidth, H: screenHeight})
+		timeTexture = getTimeTexture(renderer)
 
-		fontSurface, err := font.RenderUTF8Solid(time.Now().Format("3:04:05PM"), sdl.Color{
-			R: 0,
-			G: 0,
-			B: 0,
-			A: 255,
-		})
-		if err != nil {
-			log.Fatalln("can't render font:", err)
-		}
-
-		fontText, err := renderer.CreateTextureFromSurface(fontSurface)
-		if err != nil {
-			log.Fatalln("Error creating font texture:", err)
-		}
-		fontSurface.Free()
-
-		//fontText.Update(fontRect, fontSurface2.Pixels(), int(fontSurface2.Pitch))
-
-		renderer.Copy(fontText, nil, fontRect)
-		fontText.Destroy()
+		renderer.Copy(imageTexture, nil, fullRect)
+		renderer.Copy(timeTexture, nil, centerRect)
+		timeTexture.Destroy()
 		renderer.Present()
 
 		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
@@ -135,38 +135,19 @@ func run() (err error) {
 				running = false
 			case *sdl.TouchFingerEvent:
 				if t.Type == sdl.FINGERUP {
-					// Draw original unlock button on finger up
-					renderer.Clear()
-					renderer.Copy(imageText, nil, &sdl.Rect{X: 0, Y: 0, W: screenWidth, H: screenHeight})
-
-					fontRect.Y = textY
-					renderer.Copy(fontText, nil, fontRect)
-
-					renderer.Present()
 					touched = false
 				}
 				if t.Type == sdl.FINGERMOTION {
 					//log.Println("motion detected", t.DY)
 
+					// Calculate a rough estimate of the touch point
 					w, h := window.GetSize()
 					xTouch := float32(w) * t.X
 					yTouch := float32(h) * t.Y
-					//log.Println(int32(yTouch), int32(xTouch))
-
 					touchRect := &sdl.Rect{X: int32(xTouch), Y: int32(yTouch), W: 1, H: 1}
 
 					// Check if our touch intersects with the unlock button
-					if touchRect.HasIntersection(fontRect) {
-						//log.Println("Touch and OMG intersect. Moving OMG!")
-
-						/* Moving should be handled by the function below once dragging begins
-						renderer.Clear()
-						renderer.Copy(imageText, nil, &sdl.Rect{X: 0, Y: 0, W: 1280, H: 800})
-
-						var textY int32 = int32(yTouch)
-						renderer.Copy(fontText, nil, &sdl.Rect{X: 1280 / 2, Y: textY, W: 100, H: 50})
-						renderer.Present()
-						*/
+					if touchRect.HasIntersection(centerRect) {
 
 						// Set this to true so the next check passes
 						touched = true
@@ -177,14 +158,7 @@ func run() (err error) {
 						//log.Println("being dragged")
 						//log.Println(textY)
 
-						renderer.Clear()
-						renderer.Copy(imageText, nil, &sdl.Rect{X: 0, Y: 0, W: screenWidth, H: screenHeight})
-
 						var textY int32 = int32(yTouch)
-						fontRect.Y = textY
-						renderer.Copy(fontText, nil, fontRect)
-						renderer.Present()
-
 						touched = true
 
 						// Once dragged halfway up, exit
@@ -262,6 +236,17 @@ func main() {
 	flag.StringVar(&fontFile, "font", "sans.ttf", "Font file. TTF")
 
 	flag.Parse()
+
+	// Initialize font here, set to global font var
+	var err error
+	err = ttf.Init()
+	if err != nil {
+		log.Fatalln("Error iniitializing font lib")
+	}
+	font, err = ttf.OpenFont(fontFile, 64)
+	if err != nil {
+		log.Fatalln("can't open font:", err)
+	}
 
 	if err := run(); err != nil {
 		log.Println(err)
