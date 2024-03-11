cd8d0cdda6cb6171d6d1842cb3ba12faffa24c12
Jordan Anderson <me@jba.io>
2024-03-11 02:44:33 -0500

    Skip Golang updates for now

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+q#!/bin/bash
 
 export PATH=$PATH:/usr/local/go/bin
 
-#set -e
+set -e
 
 # Build sdl-clock in a balenalib 'cross-build' container
 # Create an accessible container from this, then copy the built binary out
@@ -22,20 +22,22 @@ export PATH=$PATH:/usr/local/go/bin
 #apt-get -y libsdl2-2.0-0 libsdl2-dev libsdl2-image-2.0-0:armhf libsdl2-image-dev:armhf libsdl2-ttf-2.0-0:armhf libsdl2-ttf-dev:armhf
 ####aptitude -y -q install libsdl2-2.0-0 libsdl2-dev libsdl2-image-2.0-0 libsdl2-image-dev libsdl2-ttf-2.0-0 libsdl2-ttf-dev &&
 
-## Install Golang
-rm -f go1.22.1.linux-armv6l.tar.gz &&
-wget --quiet https://go.dev/dl/go1.22.1.linux-armv6l.tar.gz &&
-rm -rf /usr/local/go &&
-tar -C /usr/local -xzf go1.22.1.linux-armv6l.tar.gz &&
+## Install Golang - COMMENTED OUT TO KEEP THE TOOLCHAIN STABLE ON DEBIAN/BUSTER
+##rm -f go1.22.1.linux-armv6l.tar.gz &&
+##wget --quiet https://go.dev/dl/go1.22.1.linux-armv6l.tar.gz &&
+##rm -rf /usr/local/go &&
+##tar -C /usr/local -xzf go1.22.1.linux-armv6l.tar.gz &&
 
 ## Compile
-go get -v &&
+cd sdl-clock
+go get -v
 go build -v -o sdl-clock &&
 
 go version &&
 
 ## Put into place and restart
-systemctl stop sdl-clock &&
-cp ./sdl-clock /home/sdl-clock/clock/sdl-clock &&
-chown sdl-clock: /home/sdl-clock/clock/sdl-clock &&
+systemctl stop sdl-clock
+cd ./sdl-clock/
+cp ./sdl-clock /home/sdl-clock/clock/sdl-clock
+chown sdl-clock: /home/sdl-clock/clock/sdl-clock
 systemctl restart sdl-clock
